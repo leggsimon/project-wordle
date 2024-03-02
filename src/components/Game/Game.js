a3d47d4de0d917f71d339b207785421b6baca72d
leggsimon <leggsimon@gmail.com>
2024-03-02 16:33:18 +0100

    complete exercise 5

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,6 +5,7 @@ import { WORDS } from '../../data';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import GuessForm from '../GuessForm';
 import GuessList from '../GuessList';
+import GameOverBanner from '../GameOverBanner';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -13,6 +14,10 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
+  const [gameStatus, setGameStatus] = React.useState('in_progress');
+  // const gameIsOver = guesses.length >= NUM_OF_GUESSES_ALLOWED;
+
+  console.log(gameStatus);
   return (
     <>
       <GuessList guesses={guesses} answer={answer} />
@@ -20,9 +25,37 @@ function Game() {
         disableForm={guesses.length >= NUM_OF_GUESSES_ALLOWED}
         handleGuess={(newGuess) => {
           const nextGuesses = [...guesses, { id: crypto.randomUUID(), text: newGuess }];
+
           setGuesses(nextGuesses);
+
+          if (newGuess === answer) {
+            setGameStatus('win');
+          } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+            setGameStatus('lose');
+          }
         }}
       />
+      {gameStatus !== 'in_progress' && (
+        <GameOverBanner
+          variant={gameStatus === 'win' ? 'happy' : 'sad'}
+          guessesCount={guesses.length}
+          answer={answer}
+        >
+          {gameStatus === 'win' ? (
+            <p>
+              <strong>Congratulations!</strong> Got it in{' '}
+              <strong>
+                {guesses.length} {guesses.length === 1 ? 'guess' : 'guesses'}
+              </strong>
+              .
+            </p>
+          ) : (
+            <p>
+              Sorry, the correct answer is <strong>{answer}</strong>.
+            </p>
+          )}
+        </GameOverBanner>
+      )}
     </>
   );
 }
